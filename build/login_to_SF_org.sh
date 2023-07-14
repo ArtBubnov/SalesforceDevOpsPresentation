fe7a000f49b3a3d05231d88791c120c70150a295
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-14 16:32:45 +0300

    test_08

--- build/login_to_SF_org.sh
+++ build/login_to_SF_org.sh
@@ -4,7 +4,8 @@ echo -e "Logging into Salesforce Org\n\n\n"
 
 
 echo "Step 1. Define global variables for the current pipeline"
+TARGET_BRANCH_NAME=$GITHUB_BASE_REF
 echo "Step 1 execution is finished"
 echo "Step 1 execution result:"
 echo "Target branch is:"
-echo ${{ github.event.pull_request.base.ref }}
+echo $TARGET_BRANCH_NAME=$GITHUB_BASE_REF
