e954ea7df2832536dc7f9502e0184266e9e4d892
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-17 10:46:09 +0300

    test_18

--- build/login_to_SF_org.sh
+++ build/login_to_SF_org.sh
@@ -2,16 +2,82 @@ echo -e "Logging into Salesforce Org\n\n\n"
 
 
 
-echo -e "--- Step 1. Define global variables for the current pipeline ---\n"
+echo -e "--- Step 1. Define global variables for the current pipeline ---\n\n"
 
-#SOURCE_BRANCH_NAME=$GITHUB_HEAD_REF
-#TARGET_BRANCH_NAME=$GITHUB_BASE_REF
 echo -e "Global variables display:\n"
-echo "Event type is:"
-echo -e "Pull request\n"
-echo "Source branch name is:"
-echo $SOURCE_BRANCH_NAME
+
 echo -e "\nTarget branch name is:"
 echo $TARGET_BRANCH_NAME
 
-echo -e "\n--- Step 1 execution is finished ---"
+echo -e "\n\n--- Step 1 execution is finished ---"
+
+
+
+
+echo -e "\n\n\nStep 2. Define case for the current pipeline"
+echo "Depends on the result of case definition the following will be determined:"
+echo "A - Salesforce access key (AUTH URL)"
+echo "B - Salesforce org alias"
+
+case $TARGET_BRANCH_NAME in
+    "dev")
+        CASE_LOG="dev"
+        #ACCESS_KEY_SF_DEV stored as GitHub secret variable
+        ACCESS_KEY_SF=$ACCESS_KEY_SF_DEV
+        SALESFORCE_ORG_ALIAS="salesforce_dev.org"
+        ;;
+    "qa")
+        CASE_LOG="qa"
+        #ACCESS_KEY_SF_QA stored as GitHub secret variable
+        ACCESS_KEY_SF=$ACCESS_KEY_SF_QA
+        SALESFORCE_ORG_ALIAS="salesforce_qa.org"
+        ;;
+    "uat")
+        CASE_LOG="uat"
+        #ACCESS_KEY_SF_UAT stored as GitHub secret variable
+        ACCESS_KEY_SF=$ACCESS_KEY_SF_UAT
+        SALESFORCE_ORG_ALIAS="salesforce_uat.org"
+        ;;
+    "staging")
+        CASE_LOG="staging"
+        #ACCESS_KEY_SF_STAGING stored as GitHub secret variable
+        ACCESS_KEY_SF=$ACCESS_KEY_SF_STAGING
+        SALESFORCE_ORG_ALIAS="salesforce_staging.org"        
+        ;;
+    "prod")
+        CASE_LOG="prod"
+        #ACCESS_KEY_SF_PROD stored as GitHub secret variable
+        ACCESS_KEY_SF=$ACCESS_KEY_SF_PROD
+        SALESFORCE_ORG_ALIAS="salesforce_prod.org"
+        ;;
+    *)
+        echo "Not valid"
+        echo "Target branch is not in the list (dev, qa, uat, staging, prod)
+        echo "Target branch name is:"
+        echo $TARGET_BRANCH_NAME
+        ;;
+esac
+
+echo "Step 2 execution is finished"
+echo "Step 2 execution result:"
+echo "Case result:"
+echo $CASE_LOG
+echo "Salesforce org to be used:"
+echo $CASE_LOG
+echo "Salesforce alias to be used: "
+echo $SALESFORCE_ORG_ALIAS
+
+
+
+
+echo -e "\n\n\nStep 3. Login to the target Salesforce org"
+echo "Creating .key file"
+touch access_pass.key
+
+echo -e "\nAdding access data to .key file"
+echo $ACCESS_KEY_SF > access_pass.key
+
+echo -e "\nTry SF login"
+sfdx force:auth:sfdxurl:store -f "access_pass.key" -a ${SALESFORCE_ORG_ALIAS} -d
+rm access_pass.key
+echo "Step 3 execution is finished"
