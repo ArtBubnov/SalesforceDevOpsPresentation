e3fa2b9ad80b4ebf725e53afefea6640ee9bbc58
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-20 17:11:46 +0300

    push_test_32

--- build/get_desteuctive_changes.sh
+++ build/get_desteuctive_changes.sh
@@ -1,4 +1,20 @@
-DIFF_BRANCH="origin/qa"
-GET_DESTRUCTIVE_DIFF=$(git diff --name-only --diff-filter=D ${DIFF_BRANCH} force-app/main/default)
+#mapfile -t files_array < <( git diff --name-only --diff-filter=D ${DIFF_BRANCH} force-app/main/default )
 
-echo $GET_DESTRUCTIVE_DIFF
+files_array=$ENV_DESTRUCTIVE_DIFF
+
+COUNT=0
+ARRAY_LEN=${#files_array[@]}
+LOOP_LEN=$( expr $ARRAY_LEN - 1)
+SF_COMMAND_META_STRING=""
+
+while [ $COUNT -le $LOOP_LEN ]
+do
+    CURRENT_ARRAY_NODE=${files_array[$COUNT]}
+    SF_COMMAND_META_STRING=${SF_COMMAND_META_STRING}"--source-dir "'"'${CURRENT_ARRAY_NODE}'" '
+    COUNT=$(( $COUNT +1))
+
+done
+
+
+echo "-----------TEST--------------"
+echo $SF_COMMAND_META_STRING
