e39dc1188c37ecce326961cff1ab54343bed3a52
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-20 16:59:51 +0300

    push_test_26

--- build/get_desteuctive_changes.sh
+++ build/get_desteuctive_changes.sh
@@ -1,2 +1,3 @@
-GET_DESTRUCTIVE_DIFF=$(git diff --name-only --diff-filter=D ${DIFF_BRANCH} force-app/main/default)
+GET_DESTRUCTIVE_DIFF=$(git diff --name-only --diff-filter=D qa force-app/main/default)
+
 echo $GET_DESTRUCTIVE_DIFF
