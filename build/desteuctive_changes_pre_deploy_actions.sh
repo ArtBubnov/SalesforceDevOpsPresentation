e5ea80056e680e12c3dd4da4e073521dd552bb4c
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-24 16:28:24 +0300

    push_test_99

--- build/desteuctive_changes_pre_deploy_actions.sh
+++ build/desteuctive_changes_pre_deploy_actions.sh
@@ -1,6 +1,6 @@
 echo "---------TEST-----------"
 echo $ENV_DESTRUCTIVE_DIFF_SF
-echo $ENV_DESTRUCTIVE_DIFF_SFDX
+#echo $ENV_DESTRUCTIVE_DIFF_SFDX
 
 echo "-----LS before-----"
 ls
@@ -9,13 +9,8 @@ ls
 #sf project generate manifest --name destructiveChangesManifest --source-dir "force-app/main/default/object/Account/fields/Business_Account__c.field-meta.xml" --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
 
 
-sfdx force:source:manifest:create $ENV_DESTRUCTIVE_DIFF_SFDX --manifestname zhopaManifest
-
-echo "------------"
-echo "------------"
-echo "------------"
-echo "------------"
-echo "------------"
+#sfdx force:source:manifest:create $ENV_DESTRUCTIVE_DIFF_SFDX --manifestname zhopaManifest
+#
 
 sf project generate manifest --name destructiveChangesManifest $ENV_DESTRUCTIVE_DIFF_SF --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
 echo "-----LS after-----"
