1281f6084eb1c9124bd4700242f63cf1a3fdc89c
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-24 16:31:33 +0300

    push_test_01

--- build/desteuctive_changes_pre_deploy_actions.sh
+++ build/desteuctive_changes_pre_deploy_actions.sh
@@ -5,7 +5,7 @@ echo $ENV_DESTRUCTIVE_DIFF_SF
 echo "-----LS before-----"
 ls
 #sf project generate manifest --name destructiveChangesManifest $ENV_DESTRUCTIVE_DIFF --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
-#
+# 
 #sf project generate manifest --name destructiveChangesManifest --source-dir "force-app/main/default/object/Account/fields/Business_Account__c.field-meta.xml" --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
 
 
