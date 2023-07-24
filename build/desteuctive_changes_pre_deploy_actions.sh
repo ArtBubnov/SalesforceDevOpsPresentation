274d2371e5395ff7f0f3e7b2b2f454e81268b184
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-24 16:02:29 +0300

    push_test_94

--- build/desteuctive_changes_pre_deploy_actions.sh
+++ build/desteuctive_changes_pre_deploy_actions.sh
@@ -1,14 +1,22 @@
 echo "---------TEST-----------"
-echo $ENV_DESTRUCTIVE_DIFF
+echo $ENV_DESTRUCTIVE_DIFF_SF
+echo $ENV_DESTRUCTIVE_DIFF_SFDX
 
 echo "-----LS before-----"
 ls
 #sf project generate manifest --name destructiveChangesManifest $ENV_DESTRUCTIVE_DIFF --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
-
+#
 #sf project generate manifest --name destructiveChangesManifest --source-dir "force-app/main/default/object/Account/fields/Business_Account__c.field-meta.xml" --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
 
 
-sfdx force:source:manifest:create $ENV_DESTRUCTIVE_DIFF --manifestname zhopaManifest
+sfdx force:source:manifest:create $ENV_DESTRUCTIVE_DIFF_SFDX --manifestname zhopaManifest
 
+echo "------------"
+echo "------------"
+echo "------------"
+echo "------------"
+echo "------------"
+
+sf project generate manifest --name destructiveChangesManifest $ENV_DESTRUCTIVE_DIFF_SF --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
 echo "-----LS after-----"
 ls
--- build/get_desteuctive_changes.sh
+++ build/get_desteuctive_changes.sh
@@ -1,3 +1,4 @@
+#11
 echo -e "--- Get destructive changes script executions start ---\n\n\n"
 
 
@@ -16,14 +17,28 @@ while [ $COUNT -le $LOOP_LEN ]
 do
     CURRENT_ARRAY_NODE=${files_array[$COUNT]}
     SF_COMMAND_META_STRING=${SF_COMMAND_META_STRING}"--source-dir "'"'${CURRENT_ARRAY_NODE}'" '
+    SFDX_COMMAND_META_STRING=${SF_COMMAND_META_STRING}"--sourcepath "'"'${CURRENT_ARRAY_NODE}'" '
     COUNT=$(( $COUNT +1))
 
 done
 
-echo "ENV_DESTRUCTIVE_DIFF=$SF_COMMAND_META_STRING" >> "$GITHUB_ENV"
+echo "$SF_COMMAND_META_STRING" >> "$GITHUB_ENV"
+echo "$SFDX_COMMAND_META_STRING" >> "$GITHUB_ENV"
 
 
 echo -e "\nStep 1 execution result"
 echo "destructive changes list is: "
 echo $SF_COMMAND_META_STRING
 echo -e "\n--- Step 1 execution is finished ---"
+
+
+
+
+#echo "----------TEST---------------"
+
+#sf project generate manifest --name destructiveChangesManifest --source-dir "force-app/main/default" --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
+#sf project generate manifest --name destructiveChangesManifest --source-dir "force-app/main/default/objects/Account/fields/Business_Account__c.field-meta.xml" --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
+
+
+#sfdx force:source:manifest:create $SF_COMMAND_META_STRING --manifestname zhopaManifest
+
