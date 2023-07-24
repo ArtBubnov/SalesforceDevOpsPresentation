473a09d36b964a13e865199777ed03612d594607
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-24 17:13:41 +0300

    push_test_04

--- build/desteuctive_changes_pre_deploy_actions.sh
+++ build/desteuctive_changes_pre_deploy_actions.sh
@@ -12,6 +12,8 @@ ls
 #sfdx force:source:manifest:create $ENV_DESTRUCTIVE_DIFF_SFDX --manifestname zhopaManifest
 #
 
-sf project generate manifest --name destructiveChangesManifest $ENV_DESTRUCTIVE_DIFF_SF --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
+#sf project generate manifest --name destructiveChangesManifest $ENV_DESTRUCTIVE_DIFF_SF --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
+sf project delete source $ENV_DESTRUCTIVE_DIFF_SF -c  --target-org ${SALESFORCE_ORG_ALIAS}
+
 echo "-----LS after-----"
 ls
