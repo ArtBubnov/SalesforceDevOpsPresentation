016f2f79f50bbe9690bd4d30f42abed2046fee9a
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-24 15:06:34 +0300

    push_test_81

--- build/desteuctive_changes_pre_deploy_actions.sh
+++ build/desteuctive_changes_pre_deploy_actions.sh
@@ -1,14 +1,14 @@
 echo "---------TEST-----------"
-echo $ENV_DESTRUCTIVE_DIFF
+echo $SF_COMMAND_META_STRING
 
 echo "-----LS before-----"
 ls
 #sf project generate manifest --name destructiveChangesManifest $ENV_DESTRUCTIVE_DIFF --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
 
-sf project generate manifest --name destructiveChangesManifest --source-dir "force-app/main/default/object/Account/fields/Business_Account__c.field-meta.xml" --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
+#sf project generate manifest --name destructiveChangesManifest --source-dir "force-app/main/default/object/Account/fields/Business_Account__c.field-meta.xml" --output-dir "/home/runner/work/SalesforceDevOpsPresentation/SalesforceDevOpsPresentation"
 
 
 
 
-echo "-----LS after-----"
-ls
+#echo "-----LS after-----"
+#ls
