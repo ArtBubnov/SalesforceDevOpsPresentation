c09e3ee07250b7006f64aa0f68bca73d3b2906fe
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-25 14:16:22 +0300

    push_test_19

--- build/deploy_metadata_to_sf_org.sh
+++ build/deploy_metadata_to_sf_org.sh
@@ -171,12 +171,12 @@ case $TARGET_BRANCH_NAME in
     "dev")
         echo "--- PLACEHOLDER ---.Deployment to DEV SF ENV has started"
         #TBD SHOULD BE UNCOMMENTED
-        #sfdx force:source:deploy -p "$FILES_TO_DEPLOY" -u ${SALESFORCE_TARGET_ORG_ALIAS} --loglevel WARN
+        sfdx force:source:deploy -p "$FILES_TO_DEPLOY" -u ${SALESFORCE_TARGET_ORG_ALIAS} --loglevel WARN
         ;;
     "qa")
         #echo "--- PLACEHOLDER ---.Deployment to QA SF ENV has started"
         #TBD SHOULD BE UNCOMMENTED
-        #sfdx force:source:deploy -p "$FILES_TO_DEPLOY" -u ${SALESFORCE_TARGET_ORG_ALIAS} --loglevel WARN
+        sfdx force:source:deploy -p "$FILES_TO_DEPLOY" -u ${SALESFORCE_TARGET_ORG_ALIAS} --loglevel WARN
         ;;
     "staging")
         echo "--- PLACEHOLDER ---.Deployment to STAGING SF ENV has started"
