e1600567335649c473344d582068257e9222cd4a
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-26 16:20:34 +0300

    push_test_40

--- build/login_to_SF_org.sh
+++ build/login_to_SF_org.sh
@@ -5,8 +5,10 @@ echo -e "Logging into Salesforce Org\n\n\n"
 echo -e "--- Step 1. Define global variables for the current pipeline ---\n\n"
 
 echo -e "Global variables display:\n"
+
 echo -e "\nTarget branch name is:"
 echo $TARGET_BRANCH_NAME
+echo $ACCESS_KEY_SF_DEV
 
 echo -e "\n\n--- Step 1 execution is finished ---"
 
@@ -78,7 +80,7 @@ echo $ACCESS_KEY_SF > access_pass.key
 
 echo -e "\nTry SF login"
 echo "-----TEST----"
-sfdx force:auth:sfdxurl:store -f "access_pass.key" -a ${SALESFORCE_ORG_ALIAS} -d
-#sf org login sfdx-url --sfdx-url-file "access_pass.key" --alias ${SALESFORCE_ORG_ALIAS}
+#sfdx force:auth:sfdxurl:store -f "access_pass.key" -a ${SALESFORCE_ORG_ALIAS} -d
+sf org login sfdx-url --sfdx-url-file "access_pass.key" --alias ${SALESFORCE_ORG_ALIAS}
 rm access_pass.key
 echo -e "--- Step 3.  execution is finished"
