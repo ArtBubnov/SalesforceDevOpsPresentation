8ef48d01d54c72ce63ceddf49b9e4d7f6bbfbb41
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-26 16:08:46 +0300

    push_test_40

--- build/login_to_SF_org.sh
+++ build/login_to_SF_org.sh
@@ -80,7 +80,7 @@ echo $ACCESS_KEY_SF > access_pass.key
 
 echo -e "\nTry SF login"
 echo "-----TEST----"
-sfdx force:auth:sfdxurl:store -f "access_pass.key" -a ${SALESFORCE_ORG_ALIAS} -d
-#sf org login sfdx-url --sfdx-url-file "access_pass.key" --alias ${SALESFORCE_ORG_ALIAS}
+#sfdx force:auth:sfdxurl:store -f "access_pass.key" -a ${SALESFORCE_ORG_ALIAS} -d
+sf org login sfdx-url --sfdx-url-file "access_pass.key" --alias ${SALESFORCE_ORG_ALIAS}
 rm access_pass.key
 echo -e "--- Step 3.  execution is finished"
