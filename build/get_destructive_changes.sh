dc8cbc2729f6eeb473b18348ca92e0b6b354d3a4
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-26 17:09:32 +0300

    push_test_51

--- build/get_destructive_changes.sh
+++ build/get_destructive_changes.sh
@@ -9,36 +9,76 @@ mapfile -t files_array < <( git diff --name-only --diff-filter=D ${DIFF_BRANCH} 
 
 COUNT=0
 ARRAY_LEN=${#files_array[@]}
-echo "---------TEST---------"
-echo $ARRAY_LEN
-
-
-if [[ $ARRAY_LEN != 0 ]]
-then
-  echo "TRUE"
-else
-  echo "FALSE"
- # exit 0
-  return 0
-fi
-
-
-echo "---------TEST---------"
 LOOP_LEN=$( expr $ARRAY_LEN - 1)
 SF_COMMAND_META_STRING=""
 
-while [ $COUNT -le $LOOP_LEN ]
-do
-    CURRENT_ARRAY_NODE=${files_array[$COUNT]}
-    SF_COMMAND_META_STRING=${SF_COMMAND_META_STRING}"--source-dir "'"'${CURRENT_ARRAY_NODE}'" '    
-    COUNT=$(( $COUNT +1))
+if [[ $ARRAY_LEN != 0 ]]
+    then
+        while [ $COUNT -le $LOOP_LEN ]
+        do
+            CURRENT_ARRAY_NODE=${files_array[$COUNT]}
+            SF_COMMAND_META_STRING=${SF_COMMAND_META_STRING}"--source-dir "'"'${CURRENT_ARRAY_NODE}'" '    
+            COUNT=$(( $COUNT +1))
 
-done
+        done
 
-echo "ENV_DESTRUCTIVE_DIFF_SF=$SF_COMMAND_META_STRING" >> "$GITHUB_ENV"
+        echo "ENV_DESTRUCTIVE_DIFF_SF=$SF_COMMAND_META_STRING" >> "$GITHUB_ENV"
 
 
-echo -e "\nStep 1 execution result"
-echo "destructive changes list is: "
-echo $SF_COMMAND_META_STRING
-echo -e "\n--- Step 1 execution is finished ---"
+        echo -e "\nStep 1 execution result"
+        echo "destructive changes list is: "
+        echo $SF_COMMAND_META_STRING
+        echo -e "\n--- Step 1 execution is finished ---"
+    else
+        echo "Due to there are no destructive changes"
+        echo "Script exection will be finished with 0 code status"
+        echo -e "\n--- Step 1 execution is finished ---"
+        exit 0
+fi
+
+
+
+
+
+
+
+
+
+
+
+#while [ $COUNT -le $LOOP_LEN ]
+#do
+#    CURRENT_ARRAY_NODE=${files_array[$COUNT]}
+#    SF_COMMAND_META_STRING=${SF_COMMAND_META_STRING}"--source-dir "'"'${CURRENT_ARRAY_NODE}'" '    
+#    COUNT=$(( $COUNT +1))
+
+#done
+
+#echo "ENV_DESTRUCTIVE_DIFF_SF=$SF_COMMAND_META_STRING" >> "$GITHUB_ENV"
+
+
+#echo -e "\nStep 1 execution result"
+#echo "destructive changes list is: "
+#echo $SF_COMMAND_META_STRING
+#echo -e "\n--- Step 1 execution is finished ---"
+
+
+
+
+
+
+
+#echo "---------TEST---------"
+#echo $ARRAY_LEN
+
+
+#if [[ $ARRAY_LEN != 0 ]]
+#then
+#  echo "TRUE"
+#else
+#  echo "FALSE"
+#  exit 0
+#fi
+
+
+#echo "---------TEST---------"
