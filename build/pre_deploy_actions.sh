6e2ac5cfc31be67f1a6e585c9abb66678d5595fc
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-27 14:30:11 +0300

    push_test_81

--- build/pre_deploy_actions.sh
+++ build/pre_deploy_actions.sh
@@ -1,6 +1,8 @@
 echo -e "--- Predeploy actions script executions start ---\n\n\n"
 
 
+
+
 echo -e "--- Step 1. Define global variables for the current pipeline ---\n"
 
 echo -e "Global variables display:\n"
@@ -16,108 +18,31 @@ echo -e "\n--- Step 1 execution is finished ---"
 
 
 
-echo -e "\n\n\n--- Step 2. Define case for the current pipeline ---\n"
-echo "Depends on the result of case definition the following will be determined:"
-echo "A - Target Salesforce org for metadata"
-echo -e "B - Salesforce org alias\n"
-case $TARGET_BRANCH_NAME in
-    "dev")
-        CASE_LOG="dev"
-        SALESFORCE_TARGET_ORG_ALIAS="salesforce_dev.org"
-        ;;
-    "qa")
-        CASE_LOG="qa"
-        SALESFORCE_TARGET_ORG_ALIAS="salesforce_qa.org"
-        ;;
-    "staging")
-        CASE_LOG="staging"
-        SALESFORCE_TARGET_ORG_ALIAS="salesforce_staging.org"
-        ;;
-    "uat")
-        CASE_LOG="uat"
-        SALESFORCE_TARGET_ORG_ALIAS_UAT="salesforce_uat.org"
-        ;;
-    "prod")
-        CASE_LOG="prod"
-        SALESFORCE_TARGET_ORG_ALIAS="salesforce_prod.org"
-        ;;
-    *)
-        echo "Not valid"
-        ;;
-esac
 
-echo "Step 2 execution result:"
-echo -e "\nTarget Salesforce org for metadata is:"
-echo $CASE_LOG
-echo -e "\nSalesforce org alias is:"
-echo $SALESFORCE_TARGET_ORG_ALIAS
+echo -e "\n\n\n--- Step 2. Logic execution to define the list of files to be deployed to the Salesforce org ---"
+
+echo -e "\nFind the difference between organizations"
+DIFF_BRANCH="origin/"$TARGET_BRANCH_NAME
+
+echo -e "\nDiff logic execution result:"
+GET_DIFF=$(git diff --name-only --diff-filter=ACMR ${DIFF_BRANCH} ${SALESFORCE_META_DIRECTORY})
+
+echo $GET_DIFF
+FILES_TO_DEPLOY=$(git diff --name-only --diff-filter=ACMR ${DIFF_BRANCH} ${SALESFORCE_META_DIRECTORY} | tr '\n' ',' | sed 's/\(.*\),/\1 /')
+
+
+echo -e "\nStep 2 execution is finished"
+echo "Step 3 execution result:"
+echo -e "\nFiles to deploy"
+echo $FILES_TO_DEPLOY
+
 echo -e "\n--- Step 2 execution is finished ---"
 
 
 
 
-echo -e "\n\n\n--- Step 3. Logic execution to define the list of files to be deployed to the Salesforce org ---"
-case $TARGET_BRANCH_NAME in
-    "dev")
-        echo -e "\nFind the difference between organizations"
-        DIFF_BRANCH="origin/"$TARGET_BRANCH_NAME
+echo -e "\n\n\n--- Step 3. Logic execution to define the list of apex tests to be executed during deployment to the Salesforce org ---"
 
-        echo -e "\nDiff logic execution result:"
-        GET_DIFF=$(git diff --name-only --diff-filter=ACMR ${DIFF_BRANCH} force-app/main/default)
-        echo $GET_DIFF
-        FILES_TO_DEPLOY=$(git diff --name-only --diff-filter=ACMR ${DIFF_BRANCH} force-app/main/default | tr '\n' ',' | sed 's/\(.*\),/\1 /')
-        ;;
-    "qa")
-        echo -e "\nFind the difference between organizations"
-        DIFF_BRANCH="origin/"$TARGET_BRANCH_NAME
-
-        echo -e "\nDiff logic execution result:"
-        GET_DIFF=$(git diff --name-only --diff-filter=ACMR ${DIFF_BRANCH} force-app/main/default)
-        echo $GET_DIFF
-        FILES_TO_DEPLOY=$(git diff --name-only --diff-filter=ACMR ${DIFF_BRANCH} force-app/main/default | tr '\n' ',' | sed 's/\(.*\),/\1 /')
-        ;;
-    "staging")
-        echo -e "\nFind the difference between organizations"
-        DIFF_BRANCH="origin/"$TARGET_BRANCH_NAME
-
-        echo -e "\nDiff logic execution result:"
-        GET_DIFF=$(git diff --name-only --diff-filter=ACMR ${DIFF_BRANCH} force-app/main/default)
-        echo $GET_DIFF
-        FILES_TO_DEPLOY=$(git diff --name-only --diff-filter=ACMR ${DIFF_BRANCH} force-app/main/default | tr '\n' ',' | sed 's/\(.*\),/\1 /')
-        ;;
-    "uat")
-        echo -e "\nFind the difference between organizations"
-        DIFF_BRANCH="origin/"$TARGET_BRANCH_NAME
-
-        echo -e "\nDiff logic execution result:"
-        GET_DIFF=$(git diff --name-only --diff-filter=ACMR ${DIFF_BRANCH} force-app/main/default)
-        echo $GET_DIFF
-        FILES_TO_DEPLOY=$(git diff --name-only --diff-filter=ACMR ${DIFF_BRANCH} force-app/main/default | tr '\n' ',' | sed 's/\(.*\),/\1 /')
-        ;;
-    "prod")
-        echo -e "\nFind the difference between organizations"
-        DIFF_BRANCH="origin/"$TARGET_BRANCH_NAME
-
-        echo -e "\nDiff logic execution result:"
-        GET_DIFF=$(git diff --name-only --diff-filter=ACMR ${DIFF_BRANCH} force-app/main/default)
-        echo $GET_DIFF
-        FILES_TO_DEPLOY=$(git diff --name-only --diff-filter=ACMR ${DIFF_BRANCH} force-app/main/default | tr '\n' ',' | sed 's/\(.*\),/\1 /')
-        ;;
-    *)
-        echo "Not valid"
-        ;;
-esac
-
-echo -e "\nStep 3 execution is finished"
-echo "Step 3 execution result:"
-echo -e "\nFiles to deploy"
-echo $FILES_TO_DEPLOY
-echo -e "\n--- Step 3 execution is finished ---"
-
-
-
-
-echo -e "\n\n\n--- Step 4. Logic execution to define the list of apex tests to be executed during deployment to the Salesforce org ---"
 #get to classes directory to define the list of tests to be executed
 cd force-app/main/default/classes/tests
 
@@ -154,47 +79,18 @@ NUMBER_OF_SYMBOLS_TO_TRUNCATE=$( expr $LEN_OF_LIST_OF_FILES_TO_TEST - 1 )
 LIST_OF_FILES_TO_TEST_TRUNC=$((echo ${LIST_OF_FILES_TO_TEST}) | cut -c 1-$NUMBER_OF_SYMBOLS_TO_TRUNCATE )
 
 
-echo -e "\nStep 4 execution result:"
+echo -e "\nStep 3 execution result:"
 echo -e "\nList of apex tests to be executed:"
 echo $LIST_OF_FILES_TO_TEST_TRUNC
-echo -e "\n--- Step 4 execution is finished ---"
 cd /home/runner/work/presentation/presentation
 
+echo -e "\n--- Step 3 execution is finished ---"
 
 
 
-echo -e "\n\n\n--- Step 5. Test deploy to the Salesforce org ---\n"
-echo "Depends on the target branch a spesific SFDX command should be picked"
 
+echo -e "\n\n\n--- Step 4. Test deploy to the Salesforce org ---\n"
 
-case $TARGET_BRANCH_NAME in
-    "dev")
-        echo "--- PLACEHOLDER ---.Deployment to DEV SF ENV has started"
-        #TBD SHOULD BE UNCOMMENTED
-        #sfdx force:source:deploy -p "$FILES_TO_DEPLOY" -c -l RunSpecifiedTests -r "$LIST_OF_FILES_TO_TEST_TRUNC" -u ${SALESFORCE_TARGET_ORG_ALIAS} --loglevel WARN
-        ;;
-    "qa")
-        echo "--- PLACEHOLDER ---.Deployment to QA SF ENV has started"
-        #TBD SHOULD BE UNCOMMENTED
-        #sfdx force:source:deploy -p "$FILES_TO_DEPLOY" -c -l RunSpecifiedTests -r "$LIST_OF_FILES_TO_TEST_TRUNC" -u ${SALESFORCE_TARGET_ORG_ALIAS} --loglevel WARN
-        ;;
-    "staging")
-        echo "--- PLACEHOLDER ---.Deployment to STAGING SF ENV has started"
-        #TBD SHOULD BE UNCOMMENTED
-        #sfdx force:source:deploy -p "$FILES_TO_DEPLOY" -c -l RunSpecifiedTests -r "$LIST_OF_FILES_TO_TEST_TRUNC" -u ${SALESFORCE_TARGET_ORG_ALIAS} --loglevel WARN
-        ;;
-    "uat")
-        echo "--- PLACEHOLDER ---.Deployment to UAT SF ENV has started"
-        #TBD SHOULD BE UNCOMMENTED
-        #sfdx force:source:deploy -p "$FILES_TO_DEPLOY" -c -l RunSpecifiedTests -r "$LIST_OF_FILES_TO_TEST_TRUNC" -u ${SALESFORCE_TARGET_ORG_ALIAS} --loglevel WARN
-        ;;
-    "prod")
-        echo "--- PLACEHOLDER ---.Deployment to PROD SF ENV has started"
-        #TBD SHOULD BE UNCOMMENTED
-        #sfdx force:source:deploy -p "$FILES_TO_DEPLOY" -c -l RunSpecifiedTests -r "$LIST_OF_FILES_TO_TEST_TRUNC" -u ${SALESFORCE_TARGET_ORG_ALIAS} --loglevel WARN
-        ;;
-    *)
-        echo "Not valid"
-        ;;
-esac
-echo -e "\n--- Step 5 execution is finished ---"
+sfdx force:source:deploy -p "$FILES_TO_DEPLOY" -c -l RunSpecifiedTests -r "$LIST_OF_FILES_TO_TEST_TRUNC" -u ${SALESFORCE_ORG_ALIAS} --loglevel WARN
+        
+echo -e "\n--- Step 4 execution is finished ---"
