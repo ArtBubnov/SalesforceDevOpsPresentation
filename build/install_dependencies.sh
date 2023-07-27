9bd033d829e4a92cbee3c6c9029af0b7f2543de6
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-27 15:43:52 +0300

    push_test_03

--- build/install_dependencies.sh
+++ build/install_dependencies.sh
@@ -4,6 +4,7 @@ sudo npm sfdx --version
 echo "---------TEST----------"
 echo "---------PWD-----------"
 pwd
+TEST2=$( echo pwd )
 
 echo "---------LS-----------"
 ls
@@ -14,6 +15,10 @@ echo $TEST
 echo "---------LS-2-----------"
 cd $TEST
 #cd -- "$TEST"
+ls
+
+echo "---------LS-3-----------"
+cd $TEST2
 ls
 
 #cd -- "$dirName"
