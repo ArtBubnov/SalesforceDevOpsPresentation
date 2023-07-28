608e766b75338488a69185d12974c270f7081d1b
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-28 14:15:19 +0300

    push_test_13

--- build/install_dependencies.sh
+++ build/install_dependencies.sh
@@ -2,11 +2,9 @@ echo -e "\n\n\nSalesforce CLI version check\n"
 sudo npm sfdx --version
 
 echo "---------TEST----------"
-sf project deploy start --source-dir "force-app/main/default/classes/CreatingAccount1.cls" --source-dir "force-app/main/default/classes/CreatingAccount1.cls-meta.xml" --target-org $SALESFORCE_ORG_ALIAS --test-level NoTestRun
+#works if deploy is needed
+#sf project deploy start --source-dir "force-app/main/default/classes/CreatingAccount1.cls" --source-dir "force-app/main/default/classes/CreatingAccount1.cls-meta.xml" --target-org $SALESFORCE_ORG_ALIAS --test-level NoTestRun
 
 
-#sfdx force:source:deploy -p "$FILES_TO_DEPLOY" -c -l RunSpecifiedTests -r "$LIST_OF_FILES_TO_TEST_TRUNC" -u ${SALESFORCE_ORG_ALIAS} --loglevel WARN
+echo $PR_NUMBER
 
-#sf project delete source --source-dir "force-app/main/default/classes/CreatingAccount1.cls" --source-dir "force-app/main/default/classes/CreatingAccount1.cls-meta.xml" --check-only --target-org ${SALESFORCE_ORG_ALIAS} --no-prompt
-
-#cd -- "$dirName"
