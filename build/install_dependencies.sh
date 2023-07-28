558a07b7623e2d214acf1f27c8b2e58dbc3a7221
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-07-28 17:14:21 +0300

    push_test_19

--- build/install_dependencies.sh
+++ build/install_dependencies.sh
@@ -6,7 +6,7 @@ echo "---------TEST----------"
 #sf project deploy start --source-dir "force-app/main/default/classes/CreatingAccount1.cls" --source-dir "force-app/main/default/classes/CreatingAccount1.cls-meta.xml" --target-org $SALESFORCE_ORG_ALIAS --test-level NoTestRun
 
 
-sf project deploy report --job-id 0Af5j00000TcKLhCAN
+sf project deploy report
 
 #0Af5j00000TcKLhCAN
 #0Af0x000017yLUFCA2
