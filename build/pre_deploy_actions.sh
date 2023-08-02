87c9e091cfa3b089c6d7a4c9f2a08d2e789b948f
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-08-02 15:36:32 +0300

    push_test_97

--- build/pre_deploy_actions.sh
+++ build/pre_deploy_actions.sh
@@ -45,50 +45,50 @@ echo -e "\n--- Step 2 execution is finished ---"
 
 
 
-echo -e "\n\n\n--- Step 3. Logic execution to define the list of apex tests to be executed during deployment to the Salesforce org ---"
+#echo -e "\n\n\n--- Step 3. Logic execution to define the list of apex tests to be executed during deployment to the Salesforce org ---"
 
-#get to classes directory to define the list of tests to be executed
-cd $APEX_TESTS_DIRECTORY
+##get to classes directory to define the list of tests to be executed
+#cd $APEX_TESTS_DIRECTORY
 
 #add all the files in the folder into array
-mapfile -t classes_files_array < <( ls )
+#mapfile -t classes_files_array < <( ls )
 
 #define which of the files are tests
-COUNT=0
-ARRAY_LEN=${#classes_files_array[@]}
-LIST_OF_FILES_TO_TEST=""
-LOOP_LEN=$( expr $ARRAY_LEN - 1)
+#COUNT=0
+#ARRAY_LEN=${#classes_files_array[@]}
+#LIST_OF_FILES_TO_TEST=""
+#LOOP_LEN=$( expr $ARRAY_LEN - 1)
 
-while [ $COUNT -le $LOOP_LEN ]
-do
-    if [[ ${classes_files_array[$COUNT]} == *"Test.cls"* ]];
-    then
+#while [ $COUNT -le $LOOP_LEN ]
+#do
+#    if [[ ${classes_files_array[$COUNT]} == *"Test.cls"* ]];
+#    then
 
-        if [[ ${classes_files_array[$COUNT]} == *"cls-meta.xml"* ]];
-        then
-            LIST_OF_XML_FILES=$LIST_OF_XML_FILES{classes_files_array[$COUNT]}","
-        else
-            LEN_OF_FILE_NAME=${#classes_files_array[$COUNT]}
-            NUMBER_OF_SYMBOLS_TO_TRUNCATE=$( expr $LEN_OF_FILE_NAME - 4 )
-            FILE_NAME_TRUNC=$((echo ${classes_files_array[$COUNT]}) | cut -c 1-$NUMBER_OF_SYMBOLS_TO_TRUNCATE )
-            LIST_OF_FILES_TO_TEST=$LIST_OF_FILES_TO_TEST$FILE_NAME_TRUNC","
-        fi
+#        if [[ ${classes_files_array[$COUNT]} == *"cls-meta.xml"* ]];
+#        then
+#            LIST_OF_XML_FILES=$LIST_OF_XML_FILES{classes_files_array[$COUNT]}","
+#        else
+#            LEN_OF_FILE_NAME=${#classes_files_array[$COUNT]}
+#            NUMBER_OF_SYMBOLS_TO_TRUNCATE=$( expr $LEN_OF_FILE_NAME - 4 )
+#            FILE_NAME_TRUNC=$((echo ${classes_files_array[$COUNT]}) | cut -c 1-$NUMBER_OF_SYMBOLS_TO_TRUNCATE )
+#            LIST_OF_FILES_TO_TEST=$LIST_OF_FILES_TO_TEST$FILE_NAME_TRUNC","
+#        fi
 
-    fi 
-    COUNT=$(( $COUNT +1))
-done
+#    fi 
+#    COUNT=$(( $COUNT +1))
+#done
 
-LEN_OF_LIST_OF_FILES_TO_TEST=${#LIST_OF_FILES_TO_TEST}
-NUMBER_OF_SYMBOLS_TO_TRUNCATE=$( expr $LEN_OF_LIST_OF_FILES_TO_TEST - 1 )
-LIST_OF_FILES_TO_TEST_TRUNC=$((echo ${LIST_OF_FILES_TO_TEST}) | cut -c 1-$NUMBER_OF_SYMBOLS_TO_TRUNCATE )
+#LEN_OF_LIST_OF_FILES_TO_TEST=${#LIST_OF_FILES_TO_TEST}
+#NUMBER_OF_SYMBOLS_TO_TRUNCATE=$( expr $LEN_OF_LIST_OF_FILES_TO_TEST - 1 )
+#LIST_OF_FILES_TO_TEST_TRUNC=$((echo ${LIST_OF_FILES_TO_TEST}) | cut -c 1-$NUMBER_OF_SYMBOLS_TO_TRUNCATE )
 
 
-echo -e "\nStep 3 execution result:"
-echo -e "\nList of apex tests to be executed:"
-echo $LIST_OF_FILES_TO_TEST_TRUNC
-cd $HOME_DIR
+#echo -e "\nStep 3 execution result:"
+#echo -e "\nList of apex tests to be executed:"
+#echo $LIST_OF_FILES_TO_TEST_TRUNC
+#cd $HOME_DIR
 
-echo -e "\n--- Step 3 execution is finished ---"
+#echo -e "\n--- Step 3 execution is finished ---"
 
 
 
