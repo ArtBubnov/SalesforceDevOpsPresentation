e7960a01cbba436a3b849070a34d110c2d6d9490
Artem Bubnov <artem.bubnov@vrpconsulting.com>
2023-10-27 16:17:33 +0300

    test_22

--- build/global_script.sh
+++ build/global_script.sh
@@ -1,10 +1,53 @@
 #!/bin/bash
 
-test_01 () {
-   echo -e "TEST_01"
+login_to_SF_org_full_version () {
+    echo -e "--- Logging into Salesforce Org ---\n\n\n"
+
+
+
+    echo -e "--- Step 1. Define global variables for the current pipeline ---\n\n"
+
+    echo -e "Global variables display:"
+
+    echo -e "\nTarget branch name is:"
+    echo -e "PLACEHOLDER DEV"
+    #echo $TARGET_BRANCH_NAME
+    echo -e "\nSalesforce org alias is:"
+    echo -e "PLACEHOLDER Salesforce org alias"
+    #echo $SALESFORCE_ORG_ALIAS
+
+    echo -e "\n\n--- Step 1 execution is finished ---"
+
+
+
+
+    echo -e "\n\n\nStep 2. Login to the target Salesforce org"
+
+    echo "Creating .key file"
+    #touch access_pass.key
+
+    echo -e "\nAdding access data to .key file"
+    #echo $ACCESS_KEY_SF > access_pass.key
+
+    echo -e "\nTry to login to the Salesforce org"
+    #sf org login sfdx-url --sfdx-url-file "access_pass.key" --alias ${SALESFORCE_ORG_ALIAS}
+
+    #rm access_pass.key
+
+    echo -e "\n--- Step 2.  execution is finished"
 }
 
 
-test_02 () {
-   echo -e "TEST_02"
-}
+
+
+
+
+
+#test_01 () {
+#   echo -e "TEST_01"
+#}
+
+
+#test_02 () {
+#   echo -e "TEST_02"
+#}
